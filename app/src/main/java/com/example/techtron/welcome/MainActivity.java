393b5e75146a026751655083b1573afd3a82faf2
Phillip <phillipables1@gmail.com>
2018-03-26 17:32:58 -0400

    everything has IDs now, both inner linear layouts and button are declared and instantiated in main activity

--- app/src/main/java/com/example/techtron/welcome/MainActivity.java
+++ app/src/main/java/com/example/techtron/welcome/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.techtron.welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linearLayoutTop, linearLayoutBottom;
+    Button buttonCheck;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCheck = (Button) findViewById(R.id.buttonCheck);
+        linearLayoutTop = (LinearLayout) findViewById(R.id.linearLayoutTop);
+        linearLayoutBottom = (LinearLayout) findViewById(R.id.linearLayoutBottom);
     }
 }
